52dc486f722dbe9d30a6d580e3fc59837f3557f1
ryo nakagawa <73845797+starlikeleo@users.noreply.github.com>
2023-02-26 03:05:52 +0900

    2nd_commit

--- test.py
+++ test.py
@@ -53,11 +53,45 @@ def unko_funbaru(arg):
 #     print(item)
 
 #with
-#open()
-with open('./unko.txt', 'r') as file:
-    print(file.read())
+# #open()
+# with open('./unko.txt', 'r') as file:
+#     print(file.read())
 
 #class
+# class Card:
+#     def __init__(self, date, user_name):
+#         self.date = date
+#         self.user_name = user_name
+#     def message(self):
+#         return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+
+# date_a = '2020-01-01'
+# user_name_a = 'Taro'
+# card_a = Card(date_a, user_name_a)
+
+# date_b = '2020-01-03'
+# user_name_b = 'Kayoko'
+# card_b = Card(date_b, user_name_b)
+
+# print(card_b.message())
+
+#inport
+# import math
+# print(math.pi)
+
+import numpy
+
+numpy_list = [3, 1, 5, 10, 2093, 304, 123]
+print(numpy.sum(numpy_list))
+
+
+#NumPy
+#Pandas
+#Flask
+#Django
+
+
+
 
 
 
